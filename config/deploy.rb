9b6ea4b8062208ca761421e8bbec81fd61ee875a
ponpoco <taka_ten@sun.gmobb.jp>
2019-06-09 03:47:08 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -42,7 +42,7 @@
 lock "~> 3.11.0"
 
 set :application, 'chat-space'
-set :repo_url,  'git@github.com:github.com:ponpoco999/chat-space.git'
+set :repo_url,  'https://github.com/ponpoco999/chat-space.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
@@ -50,7 +50,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['<~/.ssh/aws_key.pem']
+                  keys: ['~/.ssh/aws_key.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
