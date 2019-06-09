a1e4711b05129584199301b73e4b26403d391b92
ponpoco <taka_ten@sun.gmobb.jp>
2019-06-09 21:19:52 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,5 +13,6 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'upload-test999'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/upload-test999/*'
+  # config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/upload-test999/*'
+  config.asset_host = 'https://upload-test999.s3-ap-northeast-1.amazonaws.com'
 end
